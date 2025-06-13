0e81dc2d819e0ad66bf03464287b87324d2e08bd
weimeng.mao <weimeng.mao>
2025-06-13 13:32:40 +0800

    初始化

---
+++ src/settings.ts
@@ -0,0 +1,20 @@
+import type { RecursiveRequired, Settings } from '#/global'
+import { cloneDeep } from 'es-toolkit'
+import settingsDefault from '@/settings.default'
+import { merge } from '@/utils/object'
+
+const globalSettings: Settings.all = {
+  app: {
+    enablePermission: true,
+  },
+  menu: {
+    enableSubMenuCollapseButton: true,
+  },
+  toolbar: {
+    fullscreen: true,
+    pageReload: true,
+    colorScheme: true,
+  },
+}
+
+export default merge(globalSettings, cloneDeep(settingsDefault)) as RecursiveRequired<Settings.all>
